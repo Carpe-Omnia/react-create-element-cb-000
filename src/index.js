f65d46a094e82d796f042942fb891d451be14d3b
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-06 17:52:59 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import React from 'react' ;
 import ReactDOM from 'react-dom' ;
 
 const title = React.createElement('h1', {}, 'My First React Code');
+
+ReactDOM.render(
+  title,
+  document.getElementById('global')
+)
