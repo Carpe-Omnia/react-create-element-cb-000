c414df4df7c6e3565680ad9e637ac2ad0a2f5863
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-06 18:06:07 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const list =
-  React.createElement('div', {},
-    React.createElement('h1', {}, 'My favorite ice cream flavors'),
-    React.createElement('ul', {},
-      [
-        React.createElement('li', {}, 'Chocolate'),
-        React.createElement('li', {}, 'Vanilla'),
-        React.createElement('li', {}, 'Banana')
-      ]
-    )
-  );
+React.createElement('h1', {}, 'My favorite ice cream flavors'),
+  React.createElement('ul', {},
+    [
+      React.createElement('li', {}, 'Chocolate'),
+      React.createElement('li', {}, 'Vanilla')
+    ]
+  )
+);
 
 ReactDOM.render(
   list,
