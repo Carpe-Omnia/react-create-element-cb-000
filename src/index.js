f8f80bb393e128a1511562790efb06fd585a11ec
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-06 18:12:25 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const list =
       [
         React.createElement('li', { className: 'brown' }, 'Chocolate'),
         React.createElement('li', { className: 'white' }, 'Vanilla'),
-        React.createElement('li', { display: 'none' }, 'Banana')
+        React.createElement('li', { className: 'yellow' }, 'Banana')
       ]
     )
   );
